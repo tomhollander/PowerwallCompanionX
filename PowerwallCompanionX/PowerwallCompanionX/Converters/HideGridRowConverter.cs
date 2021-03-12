8a0ca932943b678b32dcae7fecab0e3cd57b8cfe
Tom Hollander <tomhollander@hotmail.com>
2021-03-12 19:54:24 +1100

    Bars basically working

---
+++ PowerwallCompanionX/PowerwallCompanionX/Converters/HideGridRowConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PowerwallCompanionX.Converters
+{
+    class HideGridRowConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double doubleValue = (double)value;
+            if (doubleValue < 50)
+            {
+                return new GridLength(0D);
+            }
+            return GridLength.Auto;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PowerwallCompanionX/PowerwallCompanionX/Converters/PositiveBarGraphConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PowerwallCompanionX.Converters
+{
+    class PositiveBarGraphConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double doubleValue = (double)value;
+            if (doubleValue < 0)
+            {
+                return 0D;
+            }
+            return doubleValue / 30 / (double)1; // Settings.GraphScale;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
