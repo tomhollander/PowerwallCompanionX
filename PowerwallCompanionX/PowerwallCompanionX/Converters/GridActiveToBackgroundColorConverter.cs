7157f8e8a6a98606a5f0d9d168cd7758a270eb83
Tom Hollander <tom.hollander@mongodb.com>
2021-03-14 20:10:56 +1100

    Grid Inactive background colour, store assets

---
+++ PowerwallCompanionX/PowerwallCompanionX/Converters/GridActiveToBackgroundColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PowerwallCompanionX.Converters
+{
+    class GridActiveToBackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolValue = (bool)value;
+            if (boolValue == true)
+            {
+                return Color.DimGray;
+            }
+            else
+            {
+                return Color.FromHex("#652121");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
