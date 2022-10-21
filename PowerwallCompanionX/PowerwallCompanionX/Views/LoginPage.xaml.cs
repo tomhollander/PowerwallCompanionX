126d1c725310e4bf9dc60f36c72ea0c5b9517b62
Tom Hollander <tom.hollander@mongodb.com>
2022-10-21 12:47:38 +1100

    Focus for login form

--- PowerwallCompanionX/PowerwallCompanionX/Views/LoginPage.xaml.cs
+++ PowerwallCompanionX/PowerwallCompanionX/Views/LoginPage.xaml.cs
@@ -24,6 +24,7 @@ namespace PowerwallCompanionX.Views
         protected override void OnAppearing()
         {
             webView.IsVisible = true;
+            webView.Focus();
             base.OnAppearing();
         }
 
