6b1561304f69eb51f9ab199bf1e00efda66a86ee
Tom Hollander <tomhollander@hotmail.com>
2023-10-22 11:13:00 +1100

    Weather forecast, layout fixes

---
+++ PowerwallCompanionX/PowerwallCompanionX/Converters/NullToGridLengthConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PowerwallCompanionX.Converters
+{
+    internal class NullTGridLengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ?  new GridLength(0) : GridLength.Auto;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
