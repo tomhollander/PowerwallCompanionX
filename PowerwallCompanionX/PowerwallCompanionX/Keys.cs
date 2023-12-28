7930de69a2277e66a1ead8ac500addcc76bb9254
Tom Hollander <tomhollander@hotmail.com>
2023-12-28 15:58:27 +1100

    1.40 - fleet API

--- PowerwallCompanionX/PowerwallCompanionX/Keys.cs
+++ PowerwallCompanionX/PowerwallCompanionX/Keys.cs
@@ -6,7 +6,12 @@ namespace PowerwallCompanionX
 {
     static class Keys
     {
-        public const string SyncFusion = null;
-        public const string WeatherApi = null;
+        // The app will not run with valid values for these constants. The keys used in the store app are not in the repo.
+
+        public const string SyncFusion = null; // Sign up for a free key at syncfusion.com
+        public const string WeatherApi = null; // Sign up for a free key at weatherapi.com
+        public const string TeslaAppClientId = null; // Register an app at dev.tesla.com
+        public const string TeslaAppClientSecret = null;
+        public const string TeslaAppRedirectUrl = null;
     }
 }
