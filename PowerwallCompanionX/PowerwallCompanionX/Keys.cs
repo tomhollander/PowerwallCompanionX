0fad8f7cacce3f284813801bbb9ff0446e879f60
Tom Hollander <tomhollander@hotmail.com>
2024-05-26 10:21:56 +1000

    Keys

---
+++ PowerwallCompanionX/PowerwallCompanionX/Keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerwallCompanionX
+{
+    internal class Keys
+    {
+        public const string SyncFusion = null;
+        public const string WeatherApi = null;
+        public const string TeslaAppClientId = null;
+        public const string TeslaAppClientSecret = null;
+        public const string TeslaAppRedirectUrl = null;
+        public const string AppCenterKey = null;
+    }
+}
