f70196f82e52d6abdc5918a614dd33a880927eac
Tom Hollander <tomhollander@hotmail.com>
2024-05-26 10:01:50 +1000

    Fix layout

--- PowerwallCompanionX/PowerwallCompanionX/Keys.cs
+++ PowerwallCompanionX/PowerwallCompanionX/Keys.cs
@@ -6,11 +6,11 @@ namespace PowerwallCompanionX
 {
     public static class Keys
     {
-        public const string SyncFusion = "MTE0ODU5MUAzMTM5MmUzNDJlMzBDRVg4aUpYNVJFRHRVNmU0WGVobWtLeFMydG51cXlmSmJIa0VHMWRQYVlnPQ==";
-        public const string WeatherApi = "92f1f8e860f24979b4154231233009";
-        public const string TeslaAppClientId = "d23dafb09fb8-45f2-8813-a76e0bcd0012";
-        public const string TeslaAppClientSecret = "ta-secret.I5VkrVxaPFd6jQx%";
-        public const string TeslaAppRedirectUrl = "temp://temp/temp";
-        public const string AppCenterKey = "00e4d92e-002d-4eb8-b50f-f97aa4404983";
+        public const string SyncFusion = null;
+        public const string WeatherApi = null;
+        public const string TeslaAppClientId = null;
+        public const string TeslaAppClientSecret = null;
+        public const string TeslaAppRedirectUrl = null;
+        public const string AppCenterKey = null;
     }
 }
